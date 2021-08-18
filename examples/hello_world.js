f147710b651d2db16c966a8188fe2e05cb141549
Seemann <mail@sannybuilder.com>
2021-08-18 15:15:22 -0400

    add github workflows

---
+++ examples/hello_world.js
@@ -0,0 +1,8 @@
+var VK_F5 = 116;
+
+while (true) {
+  wait(250);
+  if (isKeyPressed(VK_F5)) {
+    showTextBox("Hello world");
+  }
+}
--- hello_world.js
+++
@@ -1,8 +0,0 @@
-var VK_F5 = 116;
-
-while (true) {
-  wait(250);
-  if (isKeyPressed(VK_F5)) {
-    showTextBox("Hello world");
-  }
-}
